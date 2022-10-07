0d366c0ea3a9bea74d08c4a95f5148ae770e8f31
Charles Omukaya <102535435+Omukaya@users.noreply.github.com>
2022-10-07 12:44:41 +0300

    Update models.py
    It was too much characters

--- Blogs/models.py
+++ Blogs/models.py
@@ -3,5 +3,5 @@ from datetime import datetime
 # Create your models here.
 class Blogs(models.Model):
     title=models.CharField(max_length=100)
-    body=models.CharField(max_length=100000000)
+    body=models.CharField(max_length=800)
     date=models.DateTimeField(default=datetime.now,blank=True)
